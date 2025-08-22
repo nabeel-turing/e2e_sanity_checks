3f0b62df0a21e6d22f2fe944a6b4b37b7f6a8c1c
Nabeel Ahmad Gulzar <nabeel@Nabeels-MacBook-Pro.local>
2025-08-22 15:40:35 +0100

    passed mode arg to runner.py

--- runner.py
+++ runner.py
@@ -5,11 +5,14 @@ import pandas as pd
 orchestrator.DOCKER_IMAGE = 'gen-agents-auto-qc'
 MODE = "Colab" # 'Colab' or 'Proto'
 
+exec_config = pd.read_csv("execution_configs.csv")
+run_identifiers = list(set(exec_config['batch_id']))
+run_identifiers.sort()
+
 try:
     start_time = datetime.now()
     run_name = f'sanity_check_{start_time.strftime("%Y%m%d_%H%M%S")}'
-    run_identifiers = list(set(pd.read_csv("execution_configs.csv")['batch_id']))
-    orchestrator.run_orchestration(run_name, run_identifiers)
+    orchestrator.run_orchestration(run_name, run_identifiers, MODE)
     print(f"Finished Docker Run. Time Taken: {(datetime.now()-start_time).seconds} Seconds")
 except (FileNotFoundError, FileExistsError, ConnectionError) as e:
     print(f"\\n❌ A critical error occurred: {e}")
